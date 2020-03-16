4fff1334598fff3f0a0a71c476358dc94c21274c
Mandi Haase <haase1020@gmail.com>
2020-03-16 13:38:03 -0400

    update post-router with first get

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require('../data/db-config.js');//connection to the database
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    //get the data from the db
+    //select * from posts;
+    db.select('*').from('posts') //returns a promise
+    .then(rows => {
+        res.status(200).json({ data: rows });
+    })
+    .catch(error => {
+        res.status(500).json({ message: "sorry!" })
+    })
 });
 
 router.get('/:id', (req, res) => {
